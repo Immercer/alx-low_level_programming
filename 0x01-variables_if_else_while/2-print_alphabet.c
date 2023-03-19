9d042d4d27ac4f14ab634320802de70865a1b576
immercer <nanakkay@gamil.com>
2023-03-19 03:27:05 -0700

    list alphabets in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Return: alphabets in lowercase
+ */
+int main(void)
+{
+	int alphabet;
+
+	for (alphabet = 'a'; alphabet < 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	};
+	putchar(10);
+	return (0);
+}
