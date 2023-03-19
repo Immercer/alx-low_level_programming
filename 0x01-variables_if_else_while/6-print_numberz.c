529d311cca54aab0fa4a61a6e22099865c5a0c5e
immercer <nanakkay@gmail.com>
2023-03-19 16:18:45 +0000

    lisitng numbers wiht putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 48; number <= 57; number++)
+		putchar(number);
+	putchar(10);
+	return (0);
+}
