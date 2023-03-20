e54e5b2b9666436fff508a28b931ad89800a1fb4
immercer <nanakkay@gmail.com>
2023-03-20 14:14:13 +0000

    updat

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 /**
- *
- *
+ * main - entry point
+ * Return: Always 0
  */
 int main(void)
 {
 	int f_num;
 	int s_num;
-	for (f_num = '0'; f_num <= '9'; f_num++)
+
+	for (f_num = 0; f_num <= 8; f_num++)
 	{
-		for (s_num = '0'; s_num <= '9'; s_num++)
+		for (s_num = 0; s_num <= 9; s_num++)
 		{
-			putchar(f_num);
-			putchar(s_num);
-			if ((f_num != '9') && (s_num != '9'))
+			putchar('0' + f_num);
+			putchar('0' + s_num);
+			if (f_num != 8 || s_num != 9)
 			{
 				putchar(',');
 				putchar(' ');
