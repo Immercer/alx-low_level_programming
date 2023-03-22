b25da281b0bc9939a6274b9f11e7619a1470143b
immercer <nanakkay@gmail.com>
2023-03-22 13:53:56 +0000

    update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,16 +8,11 @@
 int print_last_digit(int n)
 {
 	if (n < 0)
-	{
 		n = -(n);
-		_putchar('0' + n);
-	}
 	else
-	{
 		n = n;
-		_putchar('0' + n);
-	}
 	n = (n % 10);
+	_putchar(n + '0');
 	return (n);
 }
 
