7243878d049c13b726f6eb757ea2fab6e432457f
immercer <nanakkay@gmail.com>
2023-03-23 22:10:05 +0000

    print a number line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_line - draws a straight line
+ * @n: lenght of line
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 1; i <= n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar(10);
+}
