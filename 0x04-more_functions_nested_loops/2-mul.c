483dae078e52cb32b5a3998174c318fd4bde896d
immercer <nanakkay@gmail.com>
2023-03-23 14:36:08 +0000

    Prototype multiplies 2 integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - function mulitplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+	int z;
+
+	z = a * b;
+	return (z);
+}
