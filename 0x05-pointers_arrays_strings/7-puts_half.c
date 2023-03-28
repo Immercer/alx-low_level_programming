cff1a053ee877797aef45ef2fe7573fc8e0afe31
immercer <nanakkay@gmail.com>
2023-03-28 13:22:42 +0000

    prints half a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strlen - print length of string
+ * @s: parameter of type pointer to char
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	c = 0;
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		c++;
+		i++;
+	}
+	return (c);
+}
+
+/**
+ * puts_half - prints half a string
+ * @str: parameter to string
+ */
+
+void puts_half(char *str)
+{
+	int i = 0;
+	int len = _strlen(str);
+
+	if (len % 2 != 0)
+	{
+		i = (len / 2) + 1;
+	}
+	else
+	{
+		i = (len / 2);
+	}
+	while (i < len)
+	{
+		_putchar(*(str + i));
+		i++;
+	}
+}
