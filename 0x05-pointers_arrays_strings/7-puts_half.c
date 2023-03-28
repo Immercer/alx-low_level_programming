62d947e3ef46e84578cde4f70394a2789ea8e7a0
immercer <nanakkay@gmail.com>
2023-03-28 13:37:19 +0000

    updatge

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -43,20 +43,3 @@ void puts_half(char *str)
 	}
 	_putchar(10);
 }
-
-
-#include "main.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char *str;
-
-    str = "123456789";
-    puts_half(str);
-    return (0);
-}
