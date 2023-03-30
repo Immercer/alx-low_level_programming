2856c766d0e49bdab368f3167a0a7fec13ee8d34
immercer <nanakkay@gmail.com>
2023-03-30 09:16:33 +0000

    udpated

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,11 +11,15 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (src[i] != '\0' && src[i] != src[n])
+	while (src[i] != '\0' && i < n)
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	dest[i] = '\0';
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
 	return (dest);
 }
