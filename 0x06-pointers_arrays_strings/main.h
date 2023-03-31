b299bd3d3b6f0444f02aea73d504c4caa286e299
immercer <nanakkay@gmail.com>
2023-03-31 04:27:30 +0000

    2 files

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,4 +6,7 @@ char *_strncpy(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
 char *string_toupper(char *s);
 char *cap_string(char *);
+char *leet(char *);
+char *cap_string(char *);
+
 #endif
