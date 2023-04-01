fc9cff35f3d8846ce26d6a6b23d1170ee5c357c5
immercer <nanakkay@gmail>
2023-04-01 19:10:43 +0000

    prints numbers

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_number - print number
+ * @n: number
+ */
+
+void print_number(int n)
+{
+	unsigned int tens, digit, positive = n;
+	double st = 1;
+
+	if (n == 0)
+		_putchar('0');
+	else
+	{
+		if (n < 0)
+		{
+			positive = n * -1;
+			_putchar('-');
+		}
+
+		while (st <= positive)
+			st *= 10;
+		tens = st / 10;
+
+		while (tens >= 1)
+		{
+			digit = positive / tens;
+			_putchar(digit + '0');
+			positive = (positive - (tens * digit));
+			tens /= 10;
+		}
+	}
+}
