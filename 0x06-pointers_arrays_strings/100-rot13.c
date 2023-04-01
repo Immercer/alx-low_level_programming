bf2498dcafd49d8a2e9bef0d6294a4b23b92551a
immercer <nanakkay@gmail>
2023-04-01 19:02:45 +0000

    update

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * rot13 - prints string encoded in rot13
@@ -7,17 +6,29 @@
  */
 char *rot13(char *s)
 {
-	int i = 0;
 
-	while (s[i] != '\0')
+	int a[53] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+		     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+		     'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+		     'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+		     'W', 'X', 'Y', 'Z'};
+	int b[53] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+		     'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+		     'l', 'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+		     'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+		     'J', 'K', 'L', 'M'};
+
+	int i, j;
+
+	for (j = 0; s[j] != '\0'; j++)
 	{
-		if (s[i] >= 65 && s[i] <= 78 || s[i] >= 97 && s[i] <= 110)
-			s[i] = s[i] + 13;
-		else if (s[i] >= 79 && s[i] <= 90 || s[i] >= 111 && s[i] <= 122)
-			s[i] = s[i] - 13;
-		else
-			s[i] = s[i];
-		i++;
+		i = 0;
+
+		while (a[i] != '\0' && s[j] != a[i])
+			i++;
+		if (s[j] == a[i]) /*if alpha matches, set to index in b arr*/
+			s[j] = b[i];
 	}
+
 	return (s);
 }
