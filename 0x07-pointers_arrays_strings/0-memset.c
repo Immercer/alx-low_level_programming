dee1eac8ab2cd80d3743db81f7fc5ea45848969a
immercer <nanakkay@gmail>
2023-04-04 09:41:00 +0000

    fills memory block with constant byte

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _memset - fills memory with a constant byte
+ * @s: memory location pointed to
+ * @b: constant byte
+ * @n: number of byte to fill
+ * Return: memory block
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	for (i = 0; i < b; i++)
+		s[i] = b;
+	return (s);
+}
