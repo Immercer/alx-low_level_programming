dba11e4824175c49b439464d337df74343bddca9
immercer <nanakkay@gmail>
2023-04-04 14:22:41 +0000

    update

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,22 +8,25 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int match = 0;
-	int i;
-	int j;
+	int i = 0, j;
+	int matches = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[i] != '\0') /*iterate through string*/
 	{
+
 		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (accept[j] == s[i])
+			if (s[i] == accept[j])
 			{
-				match++;
+				matches++;
 				break;
 			}
-			if (accept[j + 1] != '\0' && s[i] != accept[j])
-				return (match);
+			if (accept[j + 1] == '\0' && s[i] != accept[j])
+				return (matches);
 		}
+		i++;
 	}
-	return (match);
+	return (matches); /* return num if all match till end */
+
 }
+
