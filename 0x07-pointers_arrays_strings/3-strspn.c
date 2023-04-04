fcb2512c572f68d5641161f20cb7577b13e235fc
immercer <nanakkay@gmail>
2023-04-04 14:08:37 +0000

    update

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,10 +9,10 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	int match = 0;
-	int i = 0;
+	int i;
 	int j;
 
-	while (s[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
@@ -24,7 +24,6 @@ unsigned int _strspn(char *s, char *accept)
 			if (accept[j + 1] != '\0' && s[i] != accept[j])
 				return (match);
 		}
-		i++;
 	}
 	return (match);
 }
