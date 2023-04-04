7b21ce9ceecf0f964148eb4dd3611a3d2c7facc7
immercer <nanakkay@gmail>
2023-04-04 12:59:27 +0000

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strspn - searches for the first occurence of a character in a string
+ * @s: string to search
+ * @accept: string to compare
+ * Return: value
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int match = 0;
+	int i = 0;
+	int j = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				match++;
+				break;
+			}
+			if (accept[j + 1] != '\0' && s[i] != accept[j])
+				return (match);
+		}
+	}
+	return (match);
+}
