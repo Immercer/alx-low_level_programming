c1a93435588534d1ef00ce7de694e1d1827a0e14
immercer <nanakkay@gmail>
2023-04-04 14:06:13 +0000

    update

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,21 +10,19 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	int i = 0;
-	int j = 0;
+	int i;
+	int j;
 
-	while (s[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		while (accept[j] != '\0')
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (accept[j] == s[i])
+			if (s[i] == accept[j])
 			{
 				s = &s[i];
 				return (s);
 			}
-			j++;
 		}
-		i++;
 	}
 
 	return (NULL);
