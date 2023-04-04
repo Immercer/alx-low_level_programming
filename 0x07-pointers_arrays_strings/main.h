d5ecd64ae33e0f035c6e1b6422b3f419913b4f25
immercer <nanakkay@gmail>
2023-04-04 15:18:21 +0000

    update

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,5 +3,6 @@
 char *_strpbrk(char *s, char *accept);
 char *_memset(char *s, char b, unsigned int n);
 unsigned int _strspn(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 char *_strchr(char *s, char c);
 #endif
