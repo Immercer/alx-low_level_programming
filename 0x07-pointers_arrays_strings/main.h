b4fec130fe0a06b4b0ee9dd7e2fce14a4c8ed999
immercer <nanakkay@gmail>
2023-04-05 00:36:48 +0000

    sum of two diagonal matrix

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,4 +6,5 @@ char *_memset(char *s, char b, unsigned int n);
 unsigned int _strspn(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 char *_strchr(char *s, char c);
+void print_diagsums(int *a, int size);
 #endif
