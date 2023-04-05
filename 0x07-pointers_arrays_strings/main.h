5186bcb716712dba327ddc93ba763ad8ef65c15d
immercer <nanakkay@gmail>
2023-04-05 00:49:58 +0000

    update

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+int _putchar(char c);
 char *_strpbrk(char *s, char *accept);
 char *_memset(char *s, char b, unsigned int n);
 unsigned int _strspn(char *s, char *accept);
