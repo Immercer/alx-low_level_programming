147651b2791bf9ab98d9cc3c7de70a84d94109f6
immercer <nanakkay@gmail>
2023-04-05 00:17:33 +0000

    print chess board

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+void print_chessboard(char (*a)[8]);
 char *_strpbrk(char *s, char *accept);
 char *_memset(char *s, char b, unsigned int n);
 unsigned int _strspn(char *s, char *accept);
