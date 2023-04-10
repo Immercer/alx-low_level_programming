eb355e15701572ec04dd5ae2ab9a4ca530ffaa45
immercer <nanakkay@gmail>
2023-04-10 16:23:16 +0000

    Print numbers of paramters

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argv: pointers to parameters
+ * @argc: number of arguments passed
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
