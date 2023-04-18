53bb18798f3e9bd994ea17bfb663e4d16d589417
immercer <nanakkay@gmail>
2023-04-18 14:24:43 +0000

    update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,18 @@
  * @owner: owner of dog
  */
 
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - Typeof for struct dog
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
