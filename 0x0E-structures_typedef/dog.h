ece644fbdc6bcaf8263cee6bb1ec2027950ab249
immercer <nanakkay@gmail>
2023-04-18 15:36:34 +0000

    update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,24 +2,22 @@
 #define DOG_H
 
 /**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+/**
  * struct dog - define new type dog
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
  */
-
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
-/**
- * dog_t - Typeof for struct dog
- */
-typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
