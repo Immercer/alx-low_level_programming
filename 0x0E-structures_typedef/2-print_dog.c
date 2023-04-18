0f5a162680f38a8b857417677150255d992cfb82
immercer <nanakkay@gmail>
2023-04-18 13:48:46 +0000

    prints a struct dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - accesses members of struct dog and prints
+ * @d: name of struct dog
+ */
+
+void print_dog(struct dog *d)
+{
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
