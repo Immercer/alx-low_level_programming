044b86fd8b0f6e36b754cd3ea9e10512bebd10ff
immercer <nanakkay@gmail>
2023-04-18 15:46:35 +0000

    this function frees a dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <srdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - free instance of struct dog
+ * @d: to be freed
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name)
+		free(d->name);
+	if (d->owner)
+		free(d->owner);
+
+	free(d);
+
+}
