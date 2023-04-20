2bccd817e8efc6623e780f6429bf91a5940732eb
immercer <nanakkay@gmail>
2023-04-20 05:14:01 +0000

    searches for an integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,21 @@
+/**
+ * int_index - seachers for an integer
+ * @array: target array
+ * @size: size of array
+ * @cmp: comapres integers
+ * Return: -1 or found integer
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size < 0 || cmp == NULL || array == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+		if (cmp(*(array + i)))
+			return (i)
+
+	return (-1)
+}
