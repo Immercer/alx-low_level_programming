ddc882f319387e8a758563024a43df8b447072c6
immercer <nanakkay@gmail>
2023-04-20 22:55:28 +0000

    type casae

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,7 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		if (separator != NULL)
 		{
 			printf("%d", va_arg(list, int));
-			if (i != n - 1)
+			if (i != (int)n - 1)
 				printf("%s", separator);
 		}
 	}
