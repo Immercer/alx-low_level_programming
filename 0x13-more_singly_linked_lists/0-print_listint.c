1cb2eafb2431aaa8e4113561c0d2017d842befda
immercer <nanakkay@gmail>
2023-05-02 21:10:59 +0000

    update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-	int num_elem = 0;
+	size_t num_elem = 0;
 	const listint_t current_node = h;
 
 	if (current_node == NULL)
 		return (num_elem);
 	while (current_node != NULL)
 	{
+		printf("%d\n", current_node->n);
 		num_elem++;
 		current_node = current_node->next;
 	}
