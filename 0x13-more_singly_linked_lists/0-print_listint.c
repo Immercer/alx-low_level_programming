03e1d3497cb244626308c0256dfa05e894f1ed1a
immercer <nanakkay@gmail>
2023-05-02 21:04:27 +0000

    print elements of listint_t

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * print_listint -prints all the elements of the listint_t list
+ * @h: target list
+ * Return: number of elements
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int num_elem = 0;
+	const listint_t current_node = h;
+
+	if (current_node == NULL)
+		return (num_elem);
+	while (current_node != NULL)
+	{
+		num_elem++;
+		current_node = current_node->next;
+	}
+	
+	return (num_elem);
+}
+
