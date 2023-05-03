2912a697de40b924466227fa7112cf9dcb830d81
immercer <nanakkay@gmail>
2023-05-03 05:27:30 +0000

    free a listint_t

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a list
+ * @head: head
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *current_node = NULL;
+
+	if (head == NULL)
+		return (NULL);
+	while (head != NULL)
+	{
+		current_node = head;
+		head = head->next;
+		free(current_node);
+	}
+}
