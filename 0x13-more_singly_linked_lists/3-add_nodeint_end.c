5076bc1c8a8415066c43edf91a7fe483418bcb17
immercer <nanakkay@gmail>
2023-05-04 04:50:24 +0000

    update

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -26,5 +26,5 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 			current_node = current_node->next;
 		current_node->next = new_node;
 	}
-	return (current_node);
+	return (new_node);
 }
