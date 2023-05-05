96157604d5645e63d763913d83cdd5a9a4f152a4
immercer <nanakkay@gmail>
2023-05-05 00:16:02 +0000

    convert binary to decimal using bitwise operators

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: pointer to string of 0 and 1 chars
+ * Return: converted interger
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int converted = 0;
+	int idx = 0;
+
+	while (b[idx] != '\0')
+	{
+		if (b[idx] != '0' || b[idx] != '1')
+			return (0);
+
+		converted = converted << 1 | b[idx] - '0';
+		idx++;
+	}
+	return (converted);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+Implementing Bit Manipulation In C Programming
