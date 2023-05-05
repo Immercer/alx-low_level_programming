06019cf3ca821b3d49a6b4b8cd9bfb1413c1a926
immercer <nanakkay@gmail>
2023-05-05 00:40:47 +0000

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,19 +8,25 @@
 
 unsigned int binary_to_uint(const char *b)
 {
+	if (b == NULL)
+		return (0);
+
 	unsigned int converted = 0;
 	int idx = 0;
-
-	if (b == NULL)
-		return (converted);
+	int power = 1;
 
 	while (b[idx] != '\0')
-	{
-		if (b[idx] != '0' || b[idx] != '1')
-			return (converted);
+		idx += 1;
+	idx--;
 
-		converted = converted << 1 | (b[idx] - '0');
-		idx++;
+	while (idx >= 0)
+	{
+		if (b[idx] != '0' && b[idx] != '1')
+			return (0);
+
+		converted += (b[idx] - '0') * power;
+		power *= 2;
+		idx--;
 	}
 	return (converted);
 }
